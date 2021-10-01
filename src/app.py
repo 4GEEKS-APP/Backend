3772ac723d157705baddfeda0eef1e80d8eb50ec
Alex Silva Z <a.silvazapata@gmail.com>
2021-10-01 19:09:51 -0300

    Second test deploy

--- src/app.py
+++ src/app.py
@@ -4,14 +4,14 @@ from flask_migrate import Migrate
 from src.database import db
 from src.routes.user_bp import user_bp
 from src.routes.event_bp import event_bp
-from src.config import ProdConfig
+from src.config import DevConfig
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
 
 
 app = Flask(__name__)
 app.url_map.slashes = False
-app.config.from_object(ProdConfig)
+app.config.from_object(DevConfig)
 db.init_app(app)
 Migrate(app, db)
 jwt = JWTManager(app)
@@ -22,6 +22,10 @@ CORS(app)
 app.register_blueprint(user_bp, url_prefix='/users')
 #Rutas de eventos
 app.register_blueprint(event_bp, url_prefix='/events')
+
+@app.route('/')
+def home():
+    return render_template('home.html')
 
 
 if __name__ == '__main__':
